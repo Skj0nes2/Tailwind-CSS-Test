b211bcbd20f7d07fb6138814b8e0fba6c04c45b6
Skj0nes2 <aric.jones@student.msdmartinsville.org>
2024-12-13 04:22:55 +0000

    msg 2

--- files/src/tsx/componets/Header.tsx
+++ files/src/tsx/componets/Header.tsx
@@ -3,7 +3,7 @@ function Header(){
 
     return(
         <>
-            <div className="bg-emerald-500 w-50 h-50 rounded-full shadow"></div>
+            <div className="bg-emerald-500 w-50 h-50 rounded-xl shadow-2xl border-2 border-red-700"></div>
         </>
     );
 }
